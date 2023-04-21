f76774f6e56590e42a404b65a85d0a6c12c6ef0f
john cotamo <alex@192.168.0.12>
2023-04-20 22:56:23 -0500

    Routes

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send("servidor")
 })
+app.get('/new-rute', (req, res) => {
+  res.send("Hi, new route")
+})
+
+app.get('products', (req, res) => {
+  res.json({
+    name:'John',
+    year:2023
+  })
+})
 
 app.listen(port, () => {
   console.log('my port ' + port)
