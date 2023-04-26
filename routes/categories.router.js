d6ed7927ca2f403dc57082278fc09bebedc78905
john Cotamo <johncotamo@hotmail.com>
2023-04-26 08:05:46 -0500

    add router

---
+++ routes/categories.router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { faker } = require('@faker-js/faker');
+
+const router = express.Router()
+
+router.get('/categories/:categoryId/products/:productsId', (req, res) => {
+  const { categoryId, productsId } = req.params;
+  res.json({
+    categoryId,
+    productsId,
+  });
+});
+
+
+
+module.exports = router
+
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
-const producsRoter = require('./products.router');
+const producsRouter = require('./products.router');
+const userRouter = require('./user.router')
+const categorieRouter = require('./categories.router')
+
 function routerApi(app) {
-  app.use('/products', producsRoter);
+  app.use('/products', producsRouter);
+  app.use('/user', userRouter)
+  app.use('/categories', categorieRouter)
 }
 
 module.exports = routerApi;
